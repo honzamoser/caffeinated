a6ffc3f45a9dcb3f1b22fca096548177480647a8
Jan Moser <jan.moser@email.cz>
2025-03-18 23:19:05 +0100

    small changes

--- index.js
+++ index.js
@@ -3,13 +3,8 @@ function calculateCaffeineContent(initialIntake, halfLife, hoursPassed) {
 }
 
 function calculateNegligibleCaffeineContent(initialIntake, halfLife) {
-    let hoursPassed = 0;
-    let caffeineContent = initialIntake;
-    while (caffeineContent > 50) {
-        caffeineContent = calculateCaffeineContent(initialIntake, halfLife, hoursPassed);
-        hoursPassed++;
-    }
-    return hoursPassed;
+    t = (Math.log(50 / initialIntake) / Math.log(0.5)) * halfLife
+    return t
 }
 
 function run() {
@@ -19,9 +14,13 @@ function run() {
     const currentCaffeineContent = calculateCaffeineContent(caffeineIntake, 5, caffeineTime);
     const negligibleCaffeineContent = calculateNegligibleCaffeineContent(currentCaffeineContent, 5);
 
+    const time_hours = Math.round(negligibleCaffeineContent);
+
     const html = `
-        <p>Your current caffeine content is ${Math.round(currentCaffeineContent)} mg.</p>
-        ${negligibleCaffeineContent > 0 ? `<p>It will take about ${negligibleCaffeineContent} hours for the caffeine in your system to be negligible.</p>` : `<p>The caffeine in your system is already negligible.</p>`}
+        <p>Your current caffeine content is about ${Math.round(currentCaffeineContent)} mg.</p>
+        ${time_hours > 0 ? `<p>It will take about ${time_hours} hours for the caffeine in your system to be negligible.</p>` :
+            `<p>Your caffeine content is already negligible.</p>`
+        }
 
 `
 
